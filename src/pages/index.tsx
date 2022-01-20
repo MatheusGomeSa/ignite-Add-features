9e3502924acfcf1a755d1cdf9b92536a4612bfe8
blackmatman <mth.ggss@gmail.com>
2022-01-20 02:57:13 -0300

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,11 +8,11 @@ import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import { format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
-import { RichText } from 'prismic-dom'
+import { RichText, RichTextBlock } from 'prismic-reactjs';
 import Link from 'next/link';
 
 import Header from '../components/Header';
-import { Console } from 'console';
+import { Console, info } from 'console';
 import { useState } from 'react';
 
 interface Post {
@@ -34,61 +34,69 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home({ postsPagination }: HomeProps) {
-  const [resultPostPagination, setResultPostPagination] = useState<PostPagination>(postsPagination);
+export default function Home({ postsPagination }) {
+  const [resultPostPagination, setResultPostPagination] = useState<PostPagination>({
+    next_page: postsPagination.next_page,
+    results: postsPagination.results.map(posts => {
+      return {
+        uid: String(posts.uid),
+        first_publication_date: posts.first_publication_date,
+        data: {
+          title: typeof posts.data.title == 'string' ? posts.data.title : RichText.asText(posts.data.title),
+          subtitle: typeof posts.data.subtitle == 'string' ? posts.data.subtitle : RichText.asText(posts.data.subtitle),
+          author: typeof posts.data.author == 'string' ? posts.data.author : RichText.asText(posts.data.author)
+        }
+      }
+    })
+  });
   async function NextPage() {
-    const newpage: PostPagination = await window.fetch(postsPagination.next_page)
+    const newpage = await window.fetch(postsPagination.next_page)
       .then(response => response.json())
       .then(data => {
-        let info = data.results.map(Newresult => {
+        let informations = data.results.map(Newresult => {
           return {
             uid: String(Newresult.uid),
             first_publication_date: Newresult.first_publication_date,
             data: {
-              title: RichText.asText(Newresult.data.title),
-              subtitle: RichText.asText(Newresult.data.subtitle),
-              author: RichText.asText(Newresult.data.author)
+              title: typeof Newresult.data.title == 'string' ? Newresult.data.title : RichText.asText(Newresult.data.title),
+              subtitle: typeof Newresult.data.subtitle == 'string' ? Newresult.data.subtitle : RichText.asText(Newresult.data.subtitle),
+              author: typeof Newresult.data.author == 'string' ? Newresult.data.author : RichText.asText(Newresult.data.author)
             }
           }
         });
 
         return {
           next_page: data.next_page,
-          results: info
+          results: informations
         }
       })
     let result = resultPostPagination.results.concat(newpage.results)
     setResultPostPagination({
       next_page: newpage.next_page,
-      results: result,
+      results: result
     })
 
   }
   return (
     <>
       <header className={styles.header}>
-        <Link href="/">
-          <a>
-            <img src="/assets/Logo.svg" alt='logo' />
-          </a>
-        </Link>
       </header>
       <div className={styles.container}>
         <main className={styles.post} >
-          {resultPostPagination.results.map(result => (
-            <Link key={result.uid} href={`/post/${result.uid}`}>
-              <a>
-                <section className={styles.post}>
-                  <h1>{result.data.title}</h1>
-                  <div className={styles.infos}>
-                    <p>{result.data.subtitle}</p>
-                    <p><FiCalendar /> {format(new Date(result.first_publication_date), 'dd MMM yyyy', { locale: ptBR })}
-                      <FiUser /> {result.data.author}</p>
-                  </div>
-                </section>
-              </a>
-            </Link>
-          ))}
+          {resultPostPagination.results.map((result) =>
+          (
+            <a key={result.uid} href={`/post/${result.uid}`}>
+              <section className={styles.post}>
+                {result.data.title}
+                <div className={styles.infos}>
+                  <p>{result.data.subtitle}</p>
+                  <p><FiCalendar /> <p>{format(new Date(result.first_publication_date), 'dd MMM yyyy', { locale: ptBR }).toLowerCase()}</p>
+                    <FiUser /> <p>{result.data.author}</p></p>
+                </div>
+              </section>
+            </a>)
+
+          )}
           {resultPostPagination.next_page ?
             <a className={styles.loader} onClick={NextPage}>Carregar mais posts</a> : ""}
         </main>
@@ -113,9 +121,9 @@ export const getStaticProps: GetStaticProps = async () => {
       uid: String(post.uid),
       first_publication_date: post.first_publication_date,
       data: {
-        title: RichText.asText(post.data.title),
-        subtitle: RichText.asText(post.data.subtitle),
-        author: RichText.asText(post.data.author)
+        title: typeof post.data.title == 'string' ? post.data.title : RichText.asText(post.data.title),
+        subtitle: typeof post.data.subtitle == 'string' ? post.data.subtitle : RichText.asText(post.data.subtitle),
+        author: typeof post.data.author == 'string' ? post.data.author : RichText.asText(post.data.author)
       }
 
     }
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -9,11 +9,11 @@ import { FiCalendar, FiUser, FiClock } from 'react-icons/fi';
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 import { useEffect, useState } from 'react';
-import { RichText } from 'prismic-reactjs';
+import { RichText, RichTextBlock } from 'prismic-reactjs';
 import { useRouter } from 'next/router'
 
 
-interface PostProps {
+interface Post {
   first_publication_date: string | null;
   data: {
     title: string;
@@ -30,28 +30,11 @@ interface PostProps {
   };
 }
 
-interface PostsProps {
-  PostPageInformations: PostProps;
+interface PostProps {
+  post: Post;
 }
 
-export default function Post({ PostPageInformations }: PostsProps) {
-  const [Timer, SetTimer] = useState(0)
-  useEffect(() => {
-    let words = [];
-    PostPageInformations.data.content.forEach(infos => {
-      var heading = infos.heading.split(' ')
-      words = words.concat(heading);
-      infos.body.forEach(body => {
-        var bod = body.text.split(' ');
-        words = words.concat(bod);
-      })
-    })
-    SetTimer(Math.ceil(words.length / 200));
-
-  })
-
-  const [Infos, SetInfos] = useState(PostPageInformations);
-
+export default function Post({ post }) {
   const router = useRouter();
 
   if (router.isFallback) {
@@ -60,23 +43,50 @@ export default function Post({ PostPageInformations }: PostsProps) {
     )
   }
 
+  const [Infos, SetInfos] = useState({
+    first_publication_date: post.first_publication_date,
+    data: {
+      title: typeof post.data.title == 'string' ? post.data.title : RichText.asText(post.data.title),
+      banner: {
+        url: post.data.banner.url
+      },
+      author: typeof post.data.author == 'string' ? post.data.author : RichText.asText(post.data.author),
+      content: post.data.content.map((content) => {
+        return {
+          heading: content.heading,
+          body: content.body
+        }
+      })
+    }
+  });
+  //console.log(JSON.stringify(Infos, null, 2))
+  const [Timer, SetTimer] = useState(0)
+  useEffect(() => {
+    let words = [];
+    Infos.data.content.forEach(infos => {
+      var heading = infos.heading.split(' ')
+      var bod = RichText.asText(infos.body).split(' ');
+      words = words.concat(heading);
+      words = words.concat(bod);
+    })
+    SetTimer(Math.ceil(words.length / 200));
+
+  })
+
   return (<>
-    <img src={PostPageInformations.data.banner.url} alt="banner" className={styles.banner} />
+    <img src={Infos.data.banner.url} alt="banner" className={styles.banner} />
     <div className={styles.content}>
       <main >
         <h1>{Infos.data.title}</h1>
         <div>
-          <FiCalendar /> {format(new Date(PostPageInformations.first_publication_date), 'dd MMM yyyy')}
-          <FiUser /> {Infos.data.author}
-          <FiClock /> {Timer} min
+          <FiCalendar /> <p>{format(new Date(Infos.first_publication_date), 'dd MMM yyyy').toLocaleLowerCase()}</p>
+          <FiUser /> <p>{Infos.data.author}</p>
+          <FiClock /> <p>{Timer} min</p>
         </div>
         {Infos.data.content.map(content => {
           return (<>
             <h3>{content.heading}</h3>
-            {content.body.map(body => {
-              return <p>{body.text}</p>
-            }
-            )}
+            {RichText.render(content.body)}
           </>
           )
         })}
@@ -114,30 +124,59 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const prismic = getPrismicClient();
   let response = await prismic.getByUID('posts', String(slug), {});
 
-  let PostPageInformations = {
+
+
+  let post = {
+    uid: response.uid,
     first_publication_date: response.first_publication_date,
     data: {
-      title: RichText.asText(response.data.title),
+      title: response.data.title,
+      subtitle: response.data.subtitle,
       banner: {
         url: response.data.banner.url
       },
-      author: RichText.asText(response.data.author),
+      author: response.data.author,
       content: response.data.content.map((content) => {
         return {
           heading: content.heading,
-          body: content.body.map((body) => {
-            return { text: body.text }
-          }
-
-          )
+          body: content.body
         }
       })
     }
   }
+
+  // console.log(JSON.stringify(post))
   return {
-    props: {
-      PostPageInformations
-    },
+    props: { post }
+
+
+
+    /* let PostPageInformations = {
+      first_publication_date: response.first_publication_date,
+      data: {
+        title: RichText.asText(response.data.title),
+        banner: {
+          url: response.data.banner.url
+        },
+        author: RichText.asText(response.data.author),
+        content: response.data.content.map((content) => {
+          return {
+            heading: content.heading,
+            body: content.body.map((body) => {
+              return { text: body.text }
+            }
+  
+            )
+          }
+        })
+      }
+    }
+    return {
+      props: {
+        PostPageInformations
+      }
+   */
+    ,
     redirect: 60 * 60 // 1 hora
   }
 
